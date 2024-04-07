1638f25c8421e8e75791722b48a45e07b4745de5
Debasmita21 <63196707+Debasmita21@users.noreply.github.com>
2024-04-08 02:26:14 +0530

    minor css changes

--- src/component/GptSearchBar.js
+++ src/component/GptSearchBar.js
@@ -5,7 +5,7 @@ const GptSearchBar = () => {
         <form className="bg-gray-800 grid grid-cols-12 w-full md:w-1/2 rounded-md">
             <input className="p-4 m-2 col-span-10 rounded-md" type="text" placeholder="What would you like to watch today?" />
             <button className="rounded-md bg-blue-600 col-span-2
-            p-4 m-2">Search</button>
+            p-1 md:p-4 m-2">Search</button>
         </form>
     </div>
   )
